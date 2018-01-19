237a41346f026a31c9ff3e34dcaf751b67a44185
日月湖 <52061009@qq.com>
2018-01-20 01:04:45 +0800

    Update ThinkFramework.php

--- src/ThinkFramework.php
+++ src/ThinkFramework.php
@@ -22,7 +22,7 @@ class ThinkFramework extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-        if ('topthink/framework' !== $package->getPrettyName()) {
+        if ('daydayin/framework' !== $package->getPrettyName()) {
             throw new \InvalidArgumentException('Unable to install this library!');
         }
 
